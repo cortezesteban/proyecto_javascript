0d9fcf171cd4d6949af8efed44245dca84f47127
Esteban Cortez <cortez.esteban@gmail.com>
2023-10-16 12:25:02 -0300

    feature/TerceraPreentraga, se agrego archivo .json y logica fetch

--- app.js
+++ app.js
@@ -5,24 +5,24 @@
       que se compran (biblioteca), los juegos comprados se marcan, para que al recargar la pagina no se puedan volver a comprar.
 */
 
-const listaJuegos = [new Juego('Elden Ring', 430, 'Rockstar Games', 'Accion', './assets/img/elden_ring_recomendado.jpg', 'Red Dead Redemption 2 es un juego de acción y aventuras de temática occidental . Jugado desde una perspectiva en primera o tercera persona, el juego está ambientado en un entorno de mundo abierto que presenta una versión ficticia de los Estados Unidos en 1899.'),
-                     new Juego('Valheim', 480, 'Iron Gate Studio', 'Supervivencia', './assets/img/valheim_recomendado.jpg', 'Valheim tiene lugar en un mundo donde los vikingos asesinados van a demostrar que son aptos para los pasillos de Valhalla . Como tal, el jugador comienza sin nada y pronto descubre que para llegar al más allá nórdico, debe derrotar a los males que acechan a Valheim. Guiado únicamente por sus instintos y las pistas ocasionales de un cuervo.'),
-                     new Juego('RDR 2', 460, 'Rockstar Games', 'Aventura', './assets/img/rdr2_recomendado.jpg', 'Red Dead Redemption 2 es un juego de acción y aventuras de temática occidental . Jugado desde una perspectiva en primera o tercera persona, el juego está ambientado en un entorno de mundo abierto que presenta una versión ficticia de los Estados Unidos en 1899.'),
-                     new Juego('Factorio', 510, 'Wube Software', 'Estrategia', './assets/img/factorio_recomendado.jpg', 'Factorio es un videojuego de estrategia en tiempo real desarrollado por Wube Software. Ha estado disponible como videojuego de acceso anticipado desde 2013 y ha sido lanzado oficialmente el 14 de agosto de 2020.'),
-                     new Juego('New World', 530, 'Amazon Game Studios', 'Rol', './assets/img/new_world_recomendado.jpg', 'New World es un videojuego de rol multijugador masivo en línea desarrollado por Amazon Game Studios. Ambientado a mediados del siglo XVII, los jugadores tendrán la tarea de colonizar tierras modeladas a imagen y semejanza de la América Británica oriental.'),
-                     new Juego('Project Zomboid', 430, 'The Indie Stone', 'Supervivencia', './assets/img/project_zomboid_recomendados.jpg', 'En Project Zomboid , el jugador tiene como objetivo sobrevivir el mayor tiempo posible en un área apocalíptica y plagada de zombis alrededor de la ciudad de Louisville, Kentucky , conocida como Knox Country, que ha sido puesta en cuarentena por el gobierno.'),
-                     new Juego('No Man Sky', 540, 'Hello Games', 'Aventura', './assets/img/no_man_sky_recomendado.jpg', 'No Mans Sky es un juego de ciencia ficción y aventura desarrollado por Hello Games. Contará con niveles generados proceduralmente, y nos dejará explorar planetas, océanos, batallas en el espacio y luchar contra depredadores. Cada mundo tendrá su propio ecosistema, con mundos de todo tipo, desde entornos desérticos hasta lugares boscosos.'),
-                     new Juego('Star Wars Jedi', 550, 'Respawn Entertainment', 'Accion', './assets/img/star_wars_jedi_survivor_recomendado.webp', 'La historia de Cal Kestis continúa en Star Wars Jedi: Survivor, un juego de acción y aventuras en tercera persona desarrollado por Respawn Entertainment en colaboración con Lucasfilm Games. Este título para un jugador centrado en la historia retoma la aventura 5 años después de los acontecimientos de Star Wars Jedi: Fallen Order y acompañaremos a Cal en su lucha cada vez más desesperada mientras la galaxia se hunde en la oscuridad'),
-                     new Juego('Assassins Creed Odyssey', 600, 'Ubisoft Quebec', 'Aventura', './assets/img/assassins_creed _odyssey.jpg', 'Assassin Creed Odyssey es la nueva entrega de la saga de Assassin Creed tras Assassin Creed Origins. Desarrollada por Ubisoft Quebec, se trata de una vuelta de tuerca a la habitual propuesta de acción y aventura en mundo abierto de la serie, apostando en esta ocasión por una ambientación en la Antigua Grecia y un estilo de videojuego más enfocado al rol'),
-                     new Juego('Battlefield 4', 590, 'EA Games', 'Disparos', './assets/img/battlefield_4.jpg', 'Esta ocasión llevará al jugador a diferentes enclaves en Oriente. El nuevo Frostbite 3 promete un nivel gráfico y destructivo a gran escala pensando también en ver la guerra con gran realismo. Con el denominado "Levolution" la partida irá cambiando con la forma de avanzar de cada jugador.'),
-                     new Juego('Cities Skylines', 560, ' Colossal Order', 'Estrategia', './assets/img/cities_skylines.png', 'El juego es una simulación de construcción de ciudades abierta para un solo jugador. Los jugadores participan en la planificación urbana controlando la zonificación, la ubicación de las carreteras, los impuestos, los servicios públicos y el transporte público de un área'),
-                     new Juego('Counter Strike', 500, 'Valve', 'Disparos', './assets/img/counterstrike.jpg', 'Counter-Strike es la nueva versión del emblemático shooter competitivo de Valve. Se trata de un juego que llega para sustituir a CS:GO contando con el motor Source 2 e importantes cambios en físicas, humos, mecánicas de disparo, estabilidad de servidores, mapas modificados o reconstruidos desde cero y un nuevo sistema competitivo'),
-                     new Juego('Grim Dawn', 520, 'Crate Entertainment', 'Rol', './assets/img/grim_dawn.jfif', 'Grim Dawn es un juego de rol y acción con elementos de crafting en el que tendremos que sobrevivir en un mundo posapocalíptico en el que la humanidad está al borde de la extinción, y en el que la magia, ha vuelto a surgir.'),
-                     new Juego('Diablo IV', 680, 'Blizzard Entertainment', 'Rol', './assets/img/diablo_4.jpg', 'Diablo 4 es la cuarta entrega de la saga de acción y rol de Blizzard, toda una institución del mundo del videojuego. En esta ocasión volverá a sus raíces, presentándonos un mundo de terror gótico y de fantasía oscura, devolviéndonos la lucha constante contra los demonios y las criaturas que se esconden en el inframundo.'),
-                     new Juego('World of Warcraft', 610, 'Blizzard Entertainment', 'Rol', './assets/img/World_of_Warcraft.jpg', 'World of Warcraft: Classic es la versión clásica del videojuego de rol multijugador de Blizzard. El objetivo principal de la compañía es no ofrecer exactamente la versión original de 2005, sino la versión más refinada posible de esa experiencia original, pues consideran que será también la más divertida e interesante para los jugadores nostálgicos y curiosos.'),
-                     new Juego('Monster Hunter', 620, 'Niantic y Capcom', 'Accion', './assets/img/monster_hunter.jpg', 'Monster Hunter World es el retorno de la serie a las consolas de sobremesa, permitiéndonos cazar criaturas gigantes en un entorno salvaje, ya sea en solitario o con tres cazadores con un nuevo sistema multijugador drop-in que permite cooperativo entre regiones.'),
-                     new Juego('Call of Duty', 380, 'Infinity Ward', 'Disparos', './assets/img/Call_Of_Duty.jpg', 'Call of Duty: Modern Warfare 2 Remastered es la remasterización del shooter en primera persona de Activision. El videojuego ha sido actualizado por Beenox e incluye exclusivamente el modo campaña del juego original de 2009, ni su modo online ni las experiencias Spec Ops. En su momento fue un juego espectacular que repitió -e incluso superó- el éxito del primer Modern Warfare'),
-                     new Juego('Age of Empires II', 370, 'Forgotten Empires', 'Estrategia', './assets/img/Age2.webp', 'Age of Empires II: Definitive Edition celebra el vigésimo aniversario de una de las sagas de estrategia más populares y queridas del ocio digital, presentando ahora sus gráficos en 4K, remasterizando su audio y los efectos sonoros y estrenando una campaña nueva llamada The Last Khans.')];
+// const listaJuegos = [new Juego('Elden Ring', 430, 'Rockstar Games', 'Accion', './assets/img/elden_ring_recomendado.jpg', 'Red Dead Redemption 2 es un juego de acción y aventuras de temática occidental . Jugado desde una perspectiva en primera o tercera persona, el juego está ambientado en un entorno de mundo abierto que presenta una versión ficticia de los Estados Unidos en 1899.'),
+//                      new Juego('Valheim', 480, 'Iron Gate Studio', 'Supervivencia', './assets/img/valheim_recomendado.jpg', 'Valheim tiene lugar en un mundo donde los vikingos asesinados van a demostrar que son aptos para los pasillos de Valhalla . Como tal, el jugador comienza sin nada y pronto descubre que para llegar al más allá nórdico, debe derrotar a los males que acechan a Valheim. Guiado únicamente por sus instintos y las pistas ocasionales de un cuervo.'),
+//                      new Juego('RDR 2', 460, 'Rockstar Games', 'Aventura', './assets/img/rdr2_recomendado.jpg', 'Red Dead Redemption 2 es un juego de acción y aventuras de temática occidental . Jugado desde una perspectiva en primera o tercera persona, el juego está ambientado en un entorno de mundo abierto que presenta una versión ficticia de los Estados Unidos en 1899.'),
+//                      new Juego('Factorio', 510, 'Wube Software', 'Estrategia', './assets/img/factorio_recomendado.jpg', 'Factorio es un videojuego de estrategia en tiempo real desarrollado por Wube Software. Ha estado disponible como videojuego de acceso anticipado desde 2013 y ha sido lanzado oficialmente el 14 de agosto de 2020.'),
+//                      new Juego('New World', 530, 'Amazon Game Studios', 'Rol', './assets/img/new_world_recomendado.jpg', 'New World es un videojuego de rol multijugador masivo en línea desarrollado por Amazon Game Studios. Ambientado a mediados del siglo XVII, los jugadores tendrán la tarea de colonizar tierras modeladas a imagen y semejanza de la América Británica oriental.'),
+//                      new Juego('Project Zomboid', 430, 'The Indie Stone', 'Supervivencia', './assets/img/project_zomboid_recomendados.jpg', 'En Project Zomboid , el jugador tiene como objetivo sobrevivir el mayor tiempo posible en un área apocalíptica y plagada de zombis alrededor de la ciudad de Louisville, Kentucky , conocida como Knox Country, que ha sido puesta en cuarentena por el gobierno.'),
+//                      new Juego('No Man Sky', 540, 'Hello Games', 'Aventura', './assets/img/no_man_sky_recomendado.jpg', 'No Mans Sky es un juego de ciencia ficción y aventura desarrollado por Hello Games. Contará con niveles generados proceduralmente, y nos dejará explorar planetas, océanos, batallas en el espacio y luchar contra depredadores. Cada mundo tendrá su propio ecosistema, con mundos de todo tipo, desde entornos desérticos hasta lugares boscosos.'),
+//                      new Juego('Star Wars Jedi', 550, 'Respawn Entertainment', 'Accion', './assets/img/star_wars_jedi_survivor_recomendado.webp', 'La historia de Cal Kestis continúa en Star Wars Jedi: Survivor, un juego de acción y aventuras en tercera persona desarrollado por Respawn Entertainment en colaboración con Lucasfilm Games. Este título para un jugador centrado en la historia retoma la aventura 5 años después de los acontecimientos de Star Wars Jedi: Fallen Order y acompañaremos a Cal en su lucha cada vez más desesperada mientras la galaxia se hunde en la oscuridad'),
+//                      new Juego('Assassins Creed Odyssey', 600, 'Ubisoft Quebec', 'Aventura', './assets/img/assassins_creed _odyssey.jpg', 'Assassin Creed Odyssey es la nueva entrega de la saga de Assassin Creed tras Assassin Creed Origins. Desarrollada por Ubisoft Quebec, se trata de una vuelta de tuerca a la habitual propuesta de acción y aventura en mundo abierto de la serie, apostando en esta ocasión por una ambientación en la Antigua Grecia y un estilo de videojuego más enfocado al rol'),
+//                      new Juego('Battlefield 4', 590, 'EA Games', 'Disparos', './assets/img/battlefield_4.jpg', 'Esta ocasión llevará al jugador a diferentes enclaves en Oriente. El nuevo Frostbite 3 promete un nivel gráfico y destructivo a gran escala pensando también en ver la guerra con gran realismo. Con el denominado "Levolution" la partida irá cambiando con la forma de avanzar de cada jugador.'),
+//                      new Juego('Cities Skylines', 560, ' Colossal Order', 'Estrategia', './assets/img/cities_skylines.png', 'El juego es una simulación de construcción de ciudades abierta para un solo jugador. Los jugadores participan en la planificación urbana controlando la zonificación, la ubicación de las carreteras, los impuestos, los servicios públicos y el transporte público de un área'),
+//                      new Juego('Counter Strike', 500, 'Valve', 'Disparos', './assets/img/counterstrike.jpg', 'Counter-Strike es la nueva versión del emblemático shooter competitivo de Valve. Se trata de un juego que llega para sustituir a CS:GO contando con el motor Source 2 e importantes cambios en físicas, humos, mecánicas de disparo, estabilidad de servidores, mapas modificados o reconstruidos desde cero y un nuevo sistema competitivo'),
+//                      new Juego('Grim Dawn', 520, 'Crate Entertainment', 'Rol', './assets/img/grim_dawn.jfif', 'Grim Dawn es un juego de rol y acción con elementos de crafting en el que tendremos que sobrevivir en un mundo posapocalíptico en el que la humanidad está al borde de la extinción, y en el que la magia, ha vuelto a surgir.'),
+//                      new Juego('Diablo IV', 680, 'Blizzard Entertainment', 'Rol', './assets/img/diablo_4.jpg', 'Diablo 4 es la cuarta entrega de la saga de acción y rol de Blizzard, toda una institución del mundo del videojuego. En esta ocasión volverá a sus raíces, presentándonos un mundo de terror gótico y de fantasía oscura, devolviéndonos la lucha constante contra los demonios y las criaturas que se esconden en el inframundo.'),
+//                      new Juego('World of Warcraft', 610, 'Blizzard Entertainment', 'Rol', './assets/img/World_of_Warcraft.jpg', 'World of Warcraft: Classic es la versión clásica del videojuego de rol multijugador de Blizzard. El objetivo principal de la compañía es no ofrecer exactamente la versión original de 2005, sino la versión más refinada posible de esa experiencia original, pues consideran que será también la más divertida e interesante para los jugadores nostálgicos y curiosos.'),
+//                      new Juego('Monster Hunter', 620, 'Niantic y Capcom', 'Accion', './assets/img/monster_hunter.jpg', 'Monster Hunter World es el retorno de la serie a las consolas de sobremesa, permitiéndonos cazar criaturas gigantes en un entorno salvaje, ya sea en solitario o con tres cazadores con un nuevo sistema multijugador drop-in que permite cooperativo entre regiones.'),
+//                      new Juego('Call of Duty', 380, 'Infinity Ward', 'Disparos', './assets/img/Call_Of_Duty.jpg', 'Call of Duty: Modern Warfare 2 Remastered es la remasterización del shooter en primera persona de Activision. El videojuego ha sido actualizado por Beenox e incluye exclusivamente el modo campaña del juego original de 2009, ni su modo online ni las experiencias Spec Ops. En su momento fue un juego espectacular que repitió -e incluso superó- el éxito del primer Modern Warfare'),
+//                      new Juego('Age of Empires II', 370, 'Forgotten Empires', 'Estrategia', './assets/img/Age2.webp', 'Age of Empires II: Definitive Edition celebra el vigésimo aniversario de una de las sagas de estrategia más populares y queridas del ocio digital, presentando ahora sus gráficos en 4K, remasterizando su audio y los efectos sonoros y estrenando una campaña nueva llamada The Last Khans.')];
 
 let carritoJuegos = [];
 let tablaCarrito;
@@ -142,8 +142,8 @@ const realizarCompra = () => {
     tablaCarrito = ``;
 
     tablaCarrito = tablaCarrito + `
-        <h2 class="h2Tittle col-12 listaJuego text-center text-uppercase text-white mb-5 pt-4 pb-4 bg-danger">Juegos agregados al carrito</h2>
-        <table class="table table-striped table-dark col-sm-12">
+        <h2 class="h2Tittle col-12 listaJuego text-center text-uppercase text-white mb-3 pt-4 pb-4">Juegos agregados al carrito</h2>
+        <table class="table table-striped table-dark col-sm-12 listaJuego">
             <thead>
                 <tr>
                     <th scope="col"></th>
@@ -190,7 +190,15 @@ const realizarCompra = () => {
             }
             localStorage.setItem('juegosComprados', carritoJson);
             localStorage.removeItem('carritoJuegos');
-            alert('Se realizo la compra');
+
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Se realizo la compra',
+                showConfirmButton: false,
+                timer: 2000
+            });
+
             validarCarrito('comprarJuegos', '');
             carrito.innerHTML = ``;
             carritoJuegos = [];
@@ -237,4 +245,19 @@ const inicioAPP = () => {
     mostrarBusqueda(listaJuegos);
 };
 
-inicioAPP();
+const cargarLista = async () => {
+    const respuesta = await fetch('./juegos.json');
+    
+    if (respuesta.ok){
+        listaJuegos = await respuesta.json();
+        inicioAPP();
+    }
+    else{
+        const contenedor = document.getElementById('contenedorTarjetas');
+        contenedor.innerHTML = ``;
+        contenedor.innerHTML = `<p class="text-center dflex justify-content-center w-100 errorMensaje">
+                                    <span class="errorMensaje">Se produjo el siguiente error </span>Status: ${respuesta.status} - ${respuesta.statusText}</p>`;
+    }
+}
+
+cargarLista();
